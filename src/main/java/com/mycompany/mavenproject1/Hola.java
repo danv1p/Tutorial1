c48020ce6a5b50e664c65f475937de48a9abce00
Emerson Méndez Martínez <62579810+thedani40@users.noreply.github.com>
2020-04-29 01:19:17 -0600

    primeros cambios
    hola mundo

---
+++ src/main/java/com/mycompany/mavenproject1/Hola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author 1GX61LA
+ */
+public class Hola {
+    public static void main(String[] args) {
+        
+        System.out.println("Hola esto es un tutorial de como clonar en Github");
+    }
+    
+}
